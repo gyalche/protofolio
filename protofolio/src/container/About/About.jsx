27870c637ae2b99f616c62e5b44f67bb60489e54
Dawa <sherpadawa01@outlook.com>
2022-08-25 21:33:28 +0545

    working on about section and sanity react i.e sanity client and imageurlbuilder

--- protofolio/src/container/About/About.jsx
+++ protofolio/src/container/About/About.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './About.scss';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
 const About = () => {
-  return <div>About</div>;
+  const abouts = [
+    {
+      title: 'Web Development',
+      descirption: 'I am a good developer',
+      imgUrl: images.about01,
+    },
+    {
+      title: 'Javascript',
+      descirption: 'I just love it',
+      imgUrl: images.about02,
+    },
+    {
+      title: 'Frontend ',
+      descirption: 'Have been fiddeling with frontend for years',
+      imgUrl: images.about03,
+    },
+    {
+      title: 'Backend',
+      descirption: 'Been working in it for year if not years',
+      imgUrl: images.about04,
+    },
+  ];
+  return (
+    <>
+      <h2 className='head-text'>
+        I know that
+        <span> Good Development </span>
+        <br />
+        Means
+        <span> Good business</span>
+      </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className='app__profile-item'
+            key={about.title + index}>
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='p-text' style={{ marginTop: 10 }}>
+              {about.descirption}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
